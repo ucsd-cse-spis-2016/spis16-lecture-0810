e6fd19c671dd3018eaf84c639a9f7314c819dfd5
Diba Mirza <dimirza@eng.ucsd.edu>
2016-08-10 13:03:43 -0700

    Final problem in lecture

--- pilfirstdraw.py
+++ pilfirstdraw.py
@@ -1,3 +1,24 @@
+from PIL import Image
+
+def drawings():
+    pic = Image.open("butterfly.jpg")
+
+   
+    for x_col in range(pic.size[0]):
+        for x_row in range(pic.size[1]):
+            (r,g,b)= pic.getpixel((x_col,x_row))
+            if (r+g+b)/3 < 25:
+                pic.putpixel( (x_col, x_row), (255, 0, 0))
+
+
+    
+
+        
+    pic.show()
+
+
+drawings()
+
 # Draw a horizontal line across the picture on row 100
 # Generalize this so that it works for any picture
 # Draw another horizontal line that ends in the middle of the picture
