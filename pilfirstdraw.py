3f3cebc14a1514d2214b88c6801886548d0b5e15
Diba Mirza <dimirza@eng.ucsd.edu>
2016-08-10 06:43:15 -0700

    added a file

---
+++ pilfirstdraw.py
@@ -0,0 +1,12 @@
+# Draw a horizontal line across the picture on row 100
+# Generalize this so that it works for any picture
+# Draw another horizontal line that ends in the middle of the picture
+# Draw 4 horizontal lines on rows 100, 200, 300, 400 (Try again with nested loops)
+# Draw a grey horizontal bar covering the top 100 rows 
+# Draw a grey vertical bar covering the first 100 columns
+# Cover the whole picture in grey
+# Cover the top half of the picture in grey
+# Cover the top left quadrant in grey
+# Cool effects: increase the red content by 50%
+
+
